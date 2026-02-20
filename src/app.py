ceed36ff85a13f00e7639663e84c9fd12f8c6a23
Pedroregueira <pedromion20@gmail.com>
2026-02-20 08:48:26 -0300

    app.py

--- src/app.py
+++ src/app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+from parser_pdf import extrair_dados_pdf
 
+st.title("Teste Parser PDF Progad")
+
+uploaded_file = st.file_uploader("Envie o PDF", type="pdf")
+
+if uploaded_file:
+    df = extrair_dados_pdf(uploaded_file)
+    st.dataframe(df)
