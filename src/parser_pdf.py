6a83812633b55a07d664deb355ba82d06c498d55
Pedroregueira <pedromion20@gmail.com>
2026-02-20 08:50:39 -0300

    parser_pdf.py

--- src/parser_pdf.py
+++ src/parser_pdf.py
@@ -1,1 +1,12 @@
+import pdfplumber
+import pandas as pd
+import re
 
+def converter_horas_para_decimal(hora_str):
+    horas, minutos = hora_str.split(":")
+    return round(int(horas) + int(minutos) / 60, 2)
+
+def extrair_dados_pdf(file):
+    # lógica de leitura e extração
+    ...
+    return df
